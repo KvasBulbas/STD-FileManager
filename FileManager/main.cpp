b955586ef23db82737dca5d4e26276a4be63956a
kvasbulbas <shamonin.2002@mail.ru>
2024-03-30 01:19:10 +0700

    First developments

--- FileManager/main.cpp
+++ FileManager/main.cpp
@@ -1,8 +1,21 @@
 #include <QCoreApplication>
+#include <QFileInfo>
+#include <iostream>
+#include <QDebug>
+#include <QDateTime>
+#include <QTime>
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QFileInfo info("C:/QtProjects/WidgetsQT/STD-FileManager/FfLW.txt");
 
+
+    qDebug() << info.fileName();
+
+
+    if(info.completeSuffix() == "txt")
+        qDebug() << info.completeSuffix();
     return a.exec();
 }
